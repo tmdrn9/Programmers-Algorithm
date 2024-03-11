a7c3fd35969f2f5ba679bc36f8d205e8e5373af7
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-11 22:04:00 +0900

    Create 가장 큰 수.py

---
+++ 가장 큰 수.py
@@ -0,0 +1,11 @@
+'''
+" ".join(str_list):list요소들 다 합치기
+str로 처리해서 하는건 바로 파악했으나 itertools를 사용해 모든 조합을 구해서 시간초과
+힌트보고 품
+'''
+import itertools
+def solution(numbers):
+    str_li=list(map(str,numbers))
+    str_li.sort(key=lambda x: (x * 4)[:4], reverse=True) 
+    #numbers의 원소의 최대 자리수가 4개이기때문에 *4하고 4자리수까지 끊기
+    return "".join(str_li)
